babaa40d6eea47865a9ceff73cef6ab980814d2d
irish chen <shunyitian@163.com>
2016-09-14 17:14:18 +0800

    html

--- app/static/js/signup.js
+++ app/static/js/signup.js
@@ -2,15 +2,42 @@
  * Created by irish on 16/9/14.
  */
 $(function() {
-    var phone = $('.phone'), password = $('.password'), confirmPassword = $('.confirmPassword'),
-        phoneTip = $('.phoneTip'), pwdTip = $('.pwdTip'), conTip = $('.conTip');
+    var phone = $('.phone'), password = $('.password'), confirmPassword = $('.confirmPassword'), signupTip = $('.signupTip'),
+        phoneTip = $('.phoneTip'), pwdTip = $('.pwdTip'), conTip = $('.conTip'), phonevalidate = false, pwdvalidate = false, conpwdvalidate = false;
     phone.change(function () {
-        phoneTip.text(/^1(3|5|7|8)\d{9}$/.test($(this).value()) ? '' : '手机号格式不正确');
+        phonevalidate = /^1(3|5|7|8)\d{9}$/.test($(this).val());
+        phoneTip.text(phonevalidate ? '' : '手机号格式不正确');
     });
     password.change(function () {
-        // pwdTip.text()
-    })
-    $('#submit').click(function() {
-
+        pwdvalidate = $(this).val().length > 5;
+        pwdTip.text(pwdvalidate ? '' : '密码长度不能少于6位');
+        if(confirmPassword.val() != '') {
+            confirmPassword.trigger('change');
+        }
+    });
+    confirmPassword.change(function () {
+        conpwdvalidate = $(this).val() == password.val();
+        conTip.text(conpwdvalidate ? '' : '两次输入密码不相符');
+    });
+    $('#submit').click(function(e) {
+        e.preventDefault();
+        if(phonevalidate && pwdvalidate && conpwdvalidate) {
+            $.ajax({
+                url: '/account/signup',
+                type: 'post',
+                dataType: 'json',
+                data: {
+                    phone: phone.val(),
+                    password: password.val()
+                },
+                success: function(data) {
+                    if(data.code === 0) {
+                        location.replace('/static/index.html');
+                    } else {
+                        signupTip.text(data.msg);
+                    }
+                }
+            });
+        }
     });
 });
