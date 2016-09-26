06a1459be6285146359ad8fa1fc7c4e3a8389f3a
wills <weirenzhong@gmail.com>
2016-09-26 22:57:42 +0800

    .

--- app/static/js/common.js
+++ app/static/js/common.js
@@ -49,6 +49,7 @@
             dataType: 'json',
             success: function (result) {
                 if (result.code === 0) {
+                    location.replace('/static/index.html');
                     showSuccessDialog("购买成功");
                 } else if (result.code === 10006) {
                     var data = result.data.order;
