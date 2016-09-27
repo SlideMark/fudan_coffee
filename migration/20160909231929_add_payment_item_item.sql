26300578396d089156478b738a8aceac3edabb5f
wills <weirenzhong@gmail.com>
2016-09-27 16:36:47 +0800

    .

--- migration/20160909231929_add_payment_item_item.sql
+++ migration/20160909231929_add_payment_item_item.sql
@@ -1,3 +1,4 @@
 -- add_payment_item_item
-insert into fc_payment_item (name, description, money, charge) values ('50元礼包', '额外返现10元', 5000, 1000);
-insert into fc_payment_item (name, description, money, charge) values ('200元礼包', '额外返现50元', 20000, 5000);
+insert into fc_payment_item (name, description, money, charge) values ('300元礼包', '额外返现60元', 30000, 6000);
+insert into fc_payment_item (name, description, money, charge) values ('500元礼包', '额外返现130元', 50000, 13000);
+insert into fc_payment_item (name, description, money, charge) values ('1000元礼包', '额外返现280元', 100000, 28000);
