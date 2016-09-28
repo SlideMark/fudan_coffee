d2e92340406e4e011f9aa9aba3e82d47346ec026
wills <weirenzhong@gmail.com>
2016-09-28 12:30:09 +0800

    .

--- app/static/js/buy_success.js
+++ app/static/js/buy_success.js
@@ -11,7 +11,7 @@ $(function () {
             return  '<div class="order"><div class="order-hd clear"><p class="fl">购买成功：'+order.order_id+'</p></div><div class="order-bd"><span class="question">'+order.name+'</span><p>购买用户：'+order.user_name+'</p><p>微信扣费：'+order.money/100.0+'元</p><p>余额增加：'+order.balance/100.0+'元</p><p>日期：'+order.create_at+'</p></div></div>';
         }
 	}
-    var order = $('.order');
+    var order_view = $('.order');
     var order_id = getUrlParam('order_id');
     $.ajax({
         url: '/order/'+order_id,
@@ -24,7 +24,7 @@ $(function () {
                 if (order.state != 1) {
                     showTips('订单无效!');
                 } else {
-                    order.append(getOrder(result.data));
+                    order_view.append(getOrder(result.data));
                     showSuccessDialog('购买成功!');
                 }
             }
