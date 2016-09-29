e5be2fbc05f2011cf4ea1038564645af573c5b8a
wills <weirenzhong@gmail.com>
2016-09-29 22:09:22 +0900

    .

--- app/static/js/event.js
+++ app/static/js/event.js
@@ -3,7 +3,7 @@
  */
 $(function () {
     function getEvent(event) {
-        return  '<div class="event"><div class="event-hd clear"><p class="fl">'+event.title+'</p></div><div class="event-bd"><img src="'+event.poster_url+'" alt=""/><span class="question">'+event.description+'</span><p>费用：'+event.fee/100.0+'元</p><p>组织者：'+event.creator.name+'</p><p>开始时间：'+event.open_at+'</p><p>地点：'+'歌华大厦'+'</p></div><action><a href="">我要报名</></action></div>';
+        return  '<div class="event"><div class="event-hd clear"><p class="fl">'+event.title+'</p></div><div class="event-bd"><img src="'+event.poster_url+'" alt=""/><span class="question">'+event.description+'</span><p>费用：'+event.fee/100.0+'元</p><p>组织者：'+event.creator.name+'</p><p>开始时间：'+event.open_at+'</p><p>地点：'+'歌华大厦'+'</p></div><action><a onclick="join('+event.id+');">我要报名</></action></div>';
 	}
     var empty = $('.empty'), events = $('.events');
     $.ajax({
@@ -23,7 +23,7 @@ $(function () {
         }
     });
 
-    function join(event_id) {
+    function join_event(event_id) {
         $.ajax({
             url: '/event/event_id',
             type: 'post',
