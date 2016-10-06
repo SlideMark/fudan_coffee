70aba4ca66701fe40a3080d74d155f65371f3598
wills <weirenzhong@gmail.com>
2016-10-06 16:30:25 +0900

    .

--- app/static/js/event_info.js
+++ app/static/js/event_info.js
@@ -5,7 +5,7 @@
 $(function () {
     var event_id = getUrlParam('event_id');
     function getUser(user) {
-        return  '<div class="user"><div class="user-hd clear"><p class="fl">'+user.name+'</p><p class="fr">'+user.join_at+'</p></div>';
+        return  '<div class="user"><div class="user-hd clear"><img src="'+user.avator+'" class="avator fl" /><p class="fl">'+user.name+'</p><p class="fr">'+user.join_at+'</p></div>';
 	}
     var empty = $('.empty');
     var users = $('.users');
