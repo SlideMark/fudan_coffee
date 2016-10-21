8728b7f7e5e8345d9f7a43e8e0bd8c27ad61c698
wills <weirenzhong@gmail.com>
2016-10-21 15:26:16 +0800

    .

--- app/static/js/event_detail.js
+++ app/static/js/event_detail.js
@@ -4,7 +4,7 @@
 
 $(function () {
     function getEvent(event) {
-        return  '<div class="event"><div class="event-hd clear"><p class="fl">'+event.title+'</p></div><div class="event-bd"><img src="'+event.poster_url+'" alt=""/><span class="question">'+event.description+'</span><hr><p>费用：'+event.fee/100.0+'元</p><hr><p>组织者：'+event.creator.name+'</p><hr><p>已报名：'+event.num+'人</p><hr><p>活动时间：'+event.open_at+'~'+event.close_at+'</p><hr><p>地点：'+getShopName(event.shop_id)+'</p></div></div>';
+        return  '<div class="event"><div class="event-hd clear"><p class="fl">'+event.title+'</p></div><div class="event-bd"><img src="'+event.poster_url+'" alt=""/><hr><p>介绍: '+event.description+'</p><hr><p>费用：'+event.fee/100.0+'元</p><hr><p>组织者：'+event.creator.name+'</p><hr><p>已报名：'+event.num+'人</p><hr><p>活动时间：'+event.open_at+'~'+event.close_at+'</p><hr><p>地点：'+getShopName(event.shop_id)+'</p></div></div>';
 	}
     var event_id = getUrlParam('event_id');
     var code = getUrlParam('code');
